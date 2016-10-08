419cebafe9efe279ae52760ac154cb268f3a18c9
SirGFM <gfmandaji@hotmail.com>
2016-10-08 15:42:14 -0300

    Add comments to game.h

--- include-new/conf/game.h
+++ include-new/conf/game.h
@@ -6,10 +6,17 @@
 #ifndef __CONF_GAME_H__
 #define __CONF_GAME_H__
 
+/** Width of the virtual window */
 #define V_WIDTH     320
+/** Height of the virtual window */
 #define V_HEIGHT    240
+/** Organization (directory created within %APPDATA% or ~/.local/share/ where
+ * the the game's directory is located) */
 #define ORG         "com.gfmgamecorner"
+/** Game's title (used both on the window's title bar and as the actual
+ * sub-directory within ORG where logs and save data are stored) */
 #define TITLE       "JJAT+"
+/** Initial background color (only for the virtual window) */
 #define BG_COLOR    0xFF222034
 
 #endif /* __CONF_GAME_H__ */
