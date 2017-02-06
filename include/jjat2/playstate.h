41bfe71382fa7a8141de0bcb46e6fc9ada1a295a
SirGFM <gfmandaji@hotmail.com>
2017-02-05 23:07:27 -0200

    Adjust the maximum allowed level name

--- include/jjat2/playstate.h
+++ include/jjat2/playstate.h
@@ -25,7 +25,7 @@
 /** Maximum length for any level name. Since this is later modified (with prefix
  * and postfix), this length does NOT leave a free space for the trailing '\0' */
 #define MAX_VALID_LEN \
-    (MAX_LEVEL_NAME - (sizeof("levels/") - 1) - (sizeof("_bg") - 1) - (sizeof("_obj.gfm") - 1) - 1)
+    (MAX_LEVEL_NAME - (sizeof("levels/") - 1) - (sizeof("_bg_tm.gfm") - 1) - 1)
 
 struct stPlaystateCtx {
     /** The level parser */
